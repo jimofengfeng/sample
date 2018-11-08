a9c1c13da17163df30347173286dc61a99b4e41e
jimofengfeng <272226436@qq.com>
2018-11-08 11:47:04 +0800

    add account activations

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,15 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    //监听注册
+    public static function boot(){
+        parent::boot();
+
+        static::creating(function($user){
+            $user->activation_token = str_random(30);
+        });
+    }
+
     public function gravatar($size = '100')
     {
         $hash = md5(strtotime(trim($this->attributes['email'])));
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = '272226436@qq.com';
         $user->password = bcrypt('xiaomo');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
