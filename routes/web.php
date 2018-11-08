1b077b9ec9640de78d3a398d353e39a5ad0028ec
jimofengfeng <272226436@qq.com>
2018-11-08 14:43:50 +0800

    add password about

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,28 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//首页 帮助页面 关于我们
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 
+//注册页面
 Route::get('/signup', 'UsersController@create')->name('signup');
+
+//用户信息 CURD  增删改查
 Route::resource('users','UsersController');
 
+//登陆 退出
 Route::get('login','SessionsController@create')->name('login');
 Route::post('login','SessionsController@store')->name('login');
 Route::delete('logout','SessionsController@destory')->name('logout');
 
+//注册激活
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+//找回密码相关
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
